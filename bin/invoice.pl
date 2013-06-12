6575a7213b593e206253913b6212777ccb8eec25
Carlos Paredes <cparedes@covetel.com.ve>
2013-06-12 18:21:06 -0430

    Powering the template with data from the DB
    Signed-off-by: Carlos Paredes <cparedes@covetel.com.ve>

--- bin/invoice.pl
+++ bin/invoice.pl
@@ -1,35 +1,57 @@
 #!/usr/bin/env perl
 use Template;
+use strict;
+use warnings;
+use lib qw(lib);
+use Facturacion::Schema;
 
 my $tt = Template->new({
-		       INCLUDE_PATH => 'tt2/',
+		       INCLUDE_PATH => '../tt2/',
 		       INTERPOLATE => 1,
 }) || die "$Template::ERROR\n";
 
-my $vars = {
-	    invoice => {
-			id => 234,
-			date => '04-06-2013',
-			client => {
-				   name => 'Compañía Anónima de Teléfonos de Venezuela',
-				   id => 'J-29532381-6',
-				  },
-			paymentform => 'CONTADO',
-			sub_total => '123.321,00',
-			iva => 0,
-			total => '123.321,00'
-		       },
-};
+my $id = shift || die "Must indicate the invoice number";
+
+my $db =Facturacion::Schema->connect('dbi:mysql:dbname=facturacion;host=www.covetel.com.ve', 'USER', 'PASSWORD') || die $@;
+
+my @factura_items = $db->resultset('FacturaItem')->search({'id_factura' => $id});
 
 my @items;
 
-push @items,{
-	     qty => 1,
-	     desc => 'Esto corresponde la descripción del servicio, por ahora es un tanto larga, pero puede ser un tanto corta, solo son pruebas',
-	     unit_price => '123.321,00',
-	     total_price => '123.321,00',
-};
+my $tot;
+my $iva = 0;
+
+foreach my $item (@factura_items) {
+    push  @items,{
+        qty => $item->item_cant,
+        desc => $item->descripcion,
+        unit_price => $item->sub_total,
+        total_price => $item->total,
+    };
+    $tot += $item->total;
+}
+
+my @factura = $db->resultset('FacturaControl')->find({'id_factura' => $id});
+
+my $vars;
+
+foreach my $factura (@factura) {
+    $vars = {
+       invoice => {
+           id => $id,
+           date => $factura->fecha_emision,
+           client => {
+                name => $factura->id_cliente->nombre,
+                id => $factura->id_cliente->rif,
+           },
+           paymentform => $factura->metodo_pago,
+           sub_total => $tot,
+           iva => $iva,
+           total => $tot+$iva,
+       },
+    };
+}
 
 $vars->{'invoice'}->{'items'} = \@items;
 
-$tt->process('invoice.tt2', $vars, <STDOUT>);
+$tt->process('invoice.tt2', $vars) || die $tt->error(), "\n";
